9dba4b300b40239d8f6c6cf903df674ee537c741
Zaza Javakhishvili <zaza.javakhishvili@gmail.com>
2016-02-03 13:24:52 +0400

    added chattr

--- pull.py
+++ pull.py
@@ -10,8 +10,8 @@ import json
 import re
 # from pprint import pprint
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-
-
+ 
+ 
 # you can edit
 git_usr_name = 'READ_ONLY_USER'
 git_password = 'READ_ONLY_PASS'
@@ -19,12 +19,13 @@ log_dir = '/var/log/'
 log_file_name = os.path.basename(__file__) + '.log' #will be real_py_file_name .log
 default_www_dir = "/var/www/"
 default_port = 8000
+default_chattr = 'n'
 git_default_branch = 'master'
 git_default_clean = 'n'
 git_config_dir = '.git'
-
+ 
 class Webhook(BaseHTTPRequestHandler):
-
+ 
     def do_POST(self):
         message = 'OK'
         self.rfile._sock.settimeout(5)
@@ -34,13 +35,13 @@ class Webhook(BaseHTTPRequestHandler):
         self.send_header("Content-length", str(len(message)))
         self.end_headers()
         self.wfile.write(message)
-
+ 
         # parse data
         data = json.loads(data_string)
-
+ 
         # Process here sent data
         # pprint(data)
-
+ 
         if args.www_dir != default_www_dir:
             pull_www_dir = args.www_dir
             rm_rule1 = '*'
@@ -49,16 +50,18 @@ class Webhook(BaseHTTPRequestHandler):
             pull_www_dir = default_www_dir + data['repository']['name']
             rm_rule1 = '/*'
             rm_rule2 = '/.*'
-
+ 
         git_url = data['repository']['git_http_url'].replace("://",("://" + git_usr_name + ":" + git_password + "@"))
         git_branch_name = re.match('refs/heads/([0-9a-zA-Z]+)$', data['ref']).group(1)
-
+ 
         # print git_url
         # print git_branch_name
         # print pull_www_dir
-
+ 
         if git_branch_name == args.branch:
-
+            if args.chattr != default_chattr:
+                subprocess.call([("chattr -i -R " + pull_www_dir)], shell=True)
+ 
             if os.path.isdir(pull_www_dir):
                 os.chdir(pull_www_dir)
                 if os.path.isdir(git_config_dir):
@@ -99,28 +102,31 @@ class Webhook(BaseHTTPRequestHandler):
                 else:
                     log_file.write("PORT: " + str(args.port) + " CLONE-ERROR " + data['checkout_sha'] + " " + data['user_email'] + " " + time.strftime("%m-%d-%Y %H:%M:%S") + "\n")
                 log_file.close()
-
-
+               
+            if args.chattr != default_chattr:
+                subprocess.call([("chattr +i -R " + pull_www_dir)], shell=True)
+               
 if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser(description='{} description'.format(os.path.basename(__file__)))
-
+ 
     # Prepare settings
     parser.add_argument('-p', '--port', help='Server port ({} will be used by default)'.format(default_port), default=default_port, type=int)
     parser.add_argument('-w', '--www-dir', help='Server pull www dir ({} will be used by default) !!! / in end !!!'.format(default_www_dir), default=default_www_dir)
     parser.add_argument('-b', '--branch', help='Listen to branch action ({} will be used by default)'.format(git_default_branch), default=git_default_branch)
     parser.add_argument('-c', '--clean', help='Git git clean -f -d (y or n) ({} will be used by default)'.format(git_default_clean), default=git_default_clean)
-
+    parser.add_argument('-t', '--chattr', help='Remove chattr (y or n) ({} will be used by default)'.format(default_chattr), default=default_chattr)
+ 
     args = parser.parse_args()
-
+ 
     # if args.log:
-
+ 
     # Launch server
     try:
         server = HTTPServer(('', args.port), Webhook)
         os.chdir(log_dir)
         log_file = open(log_file_name, "a")
-        log_file.write("START PORT: " + str(args.port) + " MAIN_WWW_DIR: " + args.www_dir + " BRANCH: " + args.branch + " CLEAN: " +  args.clean + " TIME: " + time.strftime("%m-%d-%Y %H:%M:%S") + "\n")
+        log_file.write("START PORT: " + str(args.port) + " MAIN_WWW_DIR: " + args.www_dir + " BRANCH: " + args.branch + " CLEAN: " +  args.clean + " TIME: " + time.strftime("%m-%d-%Y %H:%M:%S") + " CHATTR " + args.chattr + "\n")
         log_file.close()
         server.serve_forever()
     except KeyboardInterrupt:
